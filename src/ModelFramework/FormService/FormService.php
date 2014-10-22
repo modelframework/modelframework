b8389d9db9c8417b6651fed97a289bf09970b954
Stanislav Burikhin <s.burikhin@cronagency.com>
2014-10-22 17:53:52 +0300

    upd

--- src/ModelFramework/FormService/FormService.php
+++ src/ModelFramework/FormService/FormService.php
@@ -8,13 +8,15 @@
 
 namespace ModelFramework\FormService;
 
+use ModelFramework\AclService\AclServiceAwareInterface;
+use ModelFramework\AclService\AclServiceAwareTrait;
 use ModelFramework\ModelConfigParserService\ModelConfigParserServiceAwareInterface;
 use ModelFramework\ModelConfigParserService\ModelConfigParserServiceAwareTrait;
 
-class FormService implements FormServiceInterface, ModelConfigParserServiceAwareInterface
+class FormService implements FormServiceInterface, ModelConfigParserServiceAwareInterface, AclServiceAwareInterface
 {
 
-    use ModelConfigParserServiceAwareTrait;
+    use ModelConfigParserServiceAwareTrait, AclServiceAwareTrait;
 
     /**
      * @param string $modelName
@@ -41,9 +43,12 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
      *
      * @return DataFormInterface
      */
-    protected function createForm( $modelName )
+    protected function createForm0( $modelName )
     {
-        return ['form' => '123'];
+        $aclModel = $this->getAclServiceVerify()->get( $modelName );
+        prn( 'Form Service', $aclModel->getAclDataVerify() );
+
+        return [ 'form' => '123' ];
 //        $modelConfig     = $this->getModelConfigParserServiceVerify()->getModelConfig( $modelName );
 //        $model           = new DataModel();
 //        $model->_fields  = $modelConfig[ 'fields' ];
@@ -55,29 +60,44 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
 //        return $model;
     }
 
-    public function getForm0( $modelName, $model, $mode )
+    /**
+     * @param string $modelName
+     *
+     * @return DataFormInterface
+     */
+    public function createForm( $modelName, $mode = 'e' )
     {
-        $cm = $this->getPermittedConfig( $modelName, $model, $mode );
+        $modelConfig = $this->getModelConfigParserServiceVerify()->getViewConfig( $modelName );
+        $aclModel    = $this->getAclServiceVerify()->get( $modelName );
+        $aclData     = $aclModel->getAclDataVerify();
+        prn( 'Form Service', $aclModel -> toArray(), $aclData, $modelConfig );
 
-        if ( $cm == null )
-        {
-            throw new \Exception( 'can\'t find configuration for the ' . $modelName . ' model ' );
-        }
+//        return [ 'form' => '123' ];
+//        $cm = $this->getPermittedConfig( $modelName, $model, $mode );
 
-        $formconfig = [
+        $formConfig = [
             'name'            => $modelName . 'Form',
             'group'           => 'form',
             'type'            => 'form',
             'options'         => [ ],
-            'attributes'      => [ 'method' => 'post', 'name' => 'form' . $modelName ], // , 'action' => 'reg'
+            'attributes'      => [ 'method' => 'post', 'name' => $modelName . 'form' ], // , 'action' => 'reg'
             'fieldsets'       => [ ],
             'elements'        => [ ],
             'validationGroup' => [ ]
         ];
         $fss        = [ ];
 
-        $_fsgroups = [ ];
-        foreach ( $cm->fields as $field_name => $field_conf )
+        prn( 'Form Config', $formConfig );
+
+        foreach( $modelConfig['fieldsets'] as $fieldSet)
+        {
+            prn($fieldSet);
+        }
+
+        exit;
+
+        $_fsGroups = [ ];
+        foreach ( $aclData->fields as $field_name => $field_conf )
         {
             $_grp = $field_conf[ 'group' ];
             if ( !isset( $_fsgroups[ $_grp ] ) )
@@ -90,7 +110,7 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
 
             foreach ( array_keys( $_element ) as $_k )
             {
-                $formconfig[ 'validationGroup' ][ $_grp ][ ] = $_k;
+                $formConfig[ 'validationGroup' ][ $_grp ][ ] = $_k;
             }
         }
 
@@ -113,7 +133,7 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
             }
             else
             {
-                $_lbl = $_fls[ 'label' ];
+                $_lbl          = $_fls[ 'label' ];
                 $_baseFieldSet = isset( $_fls[ 'base' ] ) && $_fls[ 'base' ] == true;
             }
 
@@ -139,7 +159,7 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
 //                {
 //                    $fsconfig = array_merge_recursive( $fsconfig, $this -> createFormElement( $field_name, $field_conf ) );
 //
-//                    $formconfig[ 'validaionGroup' ][ $_grp ][] = $field_name;
+//                    $formConfig[ 'validaionGroup' ][ $_grp ][] = $field_name;
 //                }
 //            }
 
@@ -147,10 +167,10 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
             $fieldset     = Obj::create( '\\Wepo\\Lib\\WepoFieldset' );
             $fss[ $_grp ] = $fieldset->parseconfig( $cfs->exchangeArray( $fsconfig ), [ ] );
 
-            $formconfig[ 'fieldsets' ][ $_grp ] = [ 'type' => $modelName . 'Fieldset' ];
+            $formConfig[ 'fieldsets' ][ $_grp ] = [ 'type' => $modelName . 'Fieldset' ];
             if ( $_baseFieldSet == true )
             {
-                $formconfig[ 'fieldsets' ][ $_grp ] = [ 'options' => [ 'use_as_base_fieldset' => true ] ];
+                $formConfig[ 'fieldsets' ][ $_grp ] = [ 'options' => [ 'use_as_base_fieldset' => true ] ];
             }
 
         }
@@ -160,17 +180,17 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
         foreach ( $utilityfs as $fieldset )
         {
             $fss[ $fieldset->getName() ]                       = $fieldset;
-            $formconfig[ 'fieldsets' ][ $fieldset->getName() ] = [ 'type' => get_class( $fieldset ) ];
+            $formConfig[ 'fieldsets' ][ $fieldset->getName() ] = [ 'type' => get_class( $fieldset ) ];
         }
 
         $cf = new ConfigForm();
-        $cf->exchangeArray( $formconfig );
+        $cf->exchangeArray( $formConfig );
         $form = Obj::create( '\\Wepo\\Lib\\WepoForm' );
 
         return $form->parseconfig( $cf, $fss );
     }
 
-    protected function getUtilityFieldsets( $modelname )
+    protected function getUtilityFieldsets( $modelName )
     {
         $fs = [ ];
 
@@ -243,5 +263,51 @@ class FormService implements FormServiceInterface, ModelConfigParserServiceAware
         return $fieldPermissions;
     }
 
+    protected function createFormElement( $name, $conf )
+    {
+        $type                                 = $conf[ 'type' ];
+        $_elementconf                         = $this->_fieldtypes[ $type ][ 'formElement' ];
+        $_elementconf[ 'options' ][ 'label' ] = isset( $conf[ 'label' ] ) ? $conf[ 'label' ] : ucfirst( $name );
+        if ( $type == 'lookup' )
+        {
+            $name .= '_id';
+            //$conf[ 'fields' ] это не совесем порядок сортировки
+            $_lall    = $this->table( $conf[ 'model' ] )->find( [ ], $conf[ 'fields' ] );
+            $_options = [ ];
+            foreach ( $_lall as $_lrow )
+            {
+                $_llabel = '';
+                $_lvalue = $_lrow->id();
+                foreach ( array_keys( $conf[ 'fields' ] ) as $_k )
+                {
+                    if ( strlen( $_llabel ) )
+                    {
+                        $_llabel .= ' ';
+                    }
+                    $_llabel .= $_lrow->$_k;
+                }
+                $_options[ $_lvalue ] = $_llabel;
+            }
+            $_elementconf[ 'options' ][ 'value_options' ] += $_options;
+        }
+        $_elementconf[ 'attributes' ][ 'name' ] = $name;
+        if ( isset( $conf[ 'required' ] ) )
+        {
+            $_elementconf[ 'attributes' ][ 'required' ] = 'required';
+            if ( isset( $_elementconf[ 'options' ][ 'label_attributes' ][ 'class' ] ) &&
+                 strlen( $_elementconf[ 'options' ][ 'label_attributes' ][ 'class' ] )
+            )
+            {
+                $_elementconf[ 'options' ][ 'label_attributes' ][ 'class' ] .= ' required';
+            }
+            else
+            {
+                $_elementconf[ 'options' ][ 'label_attributes' ] = [ 'class' => 'required' ];
+            }
+        }
+        $result = [ $name => $_elementconf ];
+
+        return $result;
+    }
 
 }
