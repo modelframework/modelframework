92b553347342661690a73f46f22a10c371844a2d
Vladimir Pasechnik <v.pasechnik@cronagency.com>
2014-11-24 18:13:35 +0200

    zzz

--- src/ModelFramework/ModelViewService/Observer/WidgetObserver.php
+++ src/ModelFramework/ModelViewService/Observer/WidgetObserver.php
@@ -189,7 +189,7 @@ class WidgetObserver
 
 //        if ( $modelName == 'Document' )
 //        {
-            prn( $modelName, $where );
+//            prn( $modelName, $where );
 //        }
         return $result;
     }
