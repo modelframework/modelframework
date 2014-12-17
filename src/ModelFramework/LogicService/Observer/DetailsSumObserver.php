546691acca0277cb6065ba9a914fbdc7a7badc3f
s.kozar <svyatoslav.kozar@gmail.com>
2014-12-17 18:02:05 +0200

    update of price calculation

--- src/ModelFramework/LogicService/Observer/DetailsSumObserver.php
+++ src/ModelFramework/LogicService/Observer/DetailsSumObserver.php
@@ -8,11 +8,25 @@
 
 namespace ModelFramework\LogicService\Observer;
 
+use ModelFramework\Utility\SplSubject\SubjectAwareTrait;
+
 class DetailsSumObserver extends AbstractObserver
 {
 
+    use SubjectAwareTrait;
+
     public function process( $model, $key, $value )
     {
-
+//        prn('heare',$model);
+        $sum = 0;
+        $details = $this->getSubject()->getGatewayService()->get( $value[ 'details' ] )
+                        ->find( [ $value[ 'extLinkF' ] => $model->$value[ 'curLinkF' ] ] );
+        foreach ( $details as $detail )
+        {
+            $sum += $detail->$value['sum_field'];
+        }
+        $model->$key = $sum;
+//        prn($sum);
+//        exit;
     }
 }
