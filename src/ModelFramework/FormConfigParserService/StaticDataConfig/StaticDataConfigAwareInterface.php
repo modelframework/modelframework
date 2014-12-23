108c933c38b8ab09a74b2cd91d03e4918ed24549
s.kozar <svyatoslav.kozar@gmail.com>
2014-12-23 22:08:07 +0200

    created static data source and static_lookup field. Use it to create static dropdown lists

---
+++ src/ModelFramework/FormConfigParserService/StaticDataConfig/StaticDataConfigAwareInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Class LogicConfigAwareInterface
+ * @package ModelFramework\DataModel\Custom
+ * @author  Vladimir Pasechnik vladimir.pasechnik@gmail.com
+ * @author  Stanislav Burikhin stanislav.burikhin@gmail.com
+ */
+
+namespace ModelFramework\LogicService\LogicConfig;
+
+interface LogicConfigAwareInterface
+{
+
+    /**
+     * @param LogicConfig $logicConfig
+     *
+     * @return $this
+     */
+    public function setLogicConfig( LogicConfig $logicConfig );
+
+    /**
+     * @return LogicConfig
+     */
+    public function getLogicConfig();
+
+    /**
+     * @return LogicConfig
+     * @throws \Exception
+     */
+    public function getLogicConfigVerify();
+}
