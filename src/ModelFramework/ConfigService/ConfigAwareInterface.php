5f6e1e9cd1bd1cd8c6a59783498689b32a231f81
Vladimir Pasechnik <v.pasechnik@cronagency.com>
2014-12-24 17:35:29 +0200

    declaration fix

--- src/ModelFramework/ConfigService/ConfigAwareInterface.php
+++ src/ModelFramework/ConfigService/ConfigAwareInterface.php
@@ -36,25 +36,29 @@ interface ConfigAwareInterface {
      * @param string $domain
      * @param string $key
      * @param null $subKey
+     * @param array $default
+     *
      *
      * @return null
      */
-    public function getConfigDomainPart( $domain, $key, $subKey = null );
+    public function getConfigDomainPart( $domain, $key, $subKey = null, $default = [] );
 
     /**
      * @param string $domain
      * @param string $key
+     * @param array $default
      *
      * @return array
      */
-    public function getConfigDomainSystem( $domain, $key = null );
+    public function getConfigDomainSystem( $domain, $key = null, $default = []  );
 
     /**
      * @param string $domain
      * @param string $key
+     * @param array $default
      *
      * @return array
      */
-    public function getConfigDomainCustom( $domain, $key = null );
+    public function getConfigDomainCustom( $domain, $key = null, $default = [] );
 
 }
