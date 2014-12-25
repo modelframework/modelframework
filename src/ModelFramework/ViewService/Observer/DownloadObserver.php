a87b55caba163ef6c73b3950f566f2eb5a44c640
prog4 <i.davidenko@cronagency.com>
2014-12-25 16:13:41 +0200

    download

--- src/ModelFramework/ViewService/Observer/DownloadObserver.php
+++ src/ModelFramework/ViewService/Observer/DownloadObserver.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class DownloadObserver
+ * Class UploadObserver
  * @package ModelFramework\ModelViewService
  * @author  Vladimir Pasechnik vladimir.pasechnik@gmail.com
  * @author  Stanislav Burikhin stanislav.burikhin@gmail.com
@@ -33,15 +33,16 @@ class DownloadObserver implements \SplObserver, ConfigAwareInterface, SubjectAwa
     public function update( \SplSubject $subject )
     {
         $this->setSubject( $subject );
+        $id = $subject->getParams()->fromRoute('id');
+
         $subject->getLogicServiceVerify()->setParams( $subject->getParams() );
 
         $dataModel = $this->initModel();
 
-//        $form = $this->initForm();
-
-        $this->process( null, $this->getModel() );
-
+        prn($id);
+        exit();
         $model = $this->setModel( $dataModel );
+
 
     }
 
@@ -68,7 +69,6 @@ class DownloadObserver implements \SplObserver, ConfigAwareInterface, SubjectAwa
                     ->process();
 
         $data = $subject->getData();
-
         if ( isset( $data[ 'model' ] ) && $data[ 'model' ] instanceof DataModelInterface )
         {
             $model = $data[ 'model' ];
@@ -117,115 +117,6 @@ class DownloadObserver implements \SplObserver, ConfigAwareInterface, SubjectAwa
         $this->getSubject()->setData( [ 'model' => $model ] );
 
         return $model;
-    }
-
-    public function initForm()
-    {
-        $subject    = $this->getSubject();
-        $viewConfig = $subject->getViewConfigVerify();
-        if ( $viewConfig->mode == 'insert' )
-        {
-            $mode = Acl::MODE_CREATE;
-        }
-
-        if ( $viewConfig->mode == 'update' )
-        {
-            $mode = Acl::MODE_EDIT;
-        }
-
-        $form = $subject->getFormServiceVerify()->get( $this->getModel(), $mode );
-        $form->setRoute( 'common' );
-        $form->setActionParams( [ 'data' => strtolower( $viewConfig->model ), 'view' => $viewConfig->mode ] );
-
-        if ( $this->getModel()->id() !== '' )
-        {
-            $form->setActionParams( [ 'id' => $this->getModel()->id() ] );
-        }
-
-        if ( isset( $form->getFieldsets()[ 'saurl' ] ) )
-        {
-            $form->getFieldsets()[ 'saurl' ]->get( 'back' )->setValue( $subject->getParams()
-                                                                               ->fromQuery( 'back', 'home' ) );
-        }
-
-        return $form;
-    }
-
-
-    /**
-     * @param $form
-     * @param $model
-     */
-    public function process( $form, $model )
-    {
-        $subject    = $this->getSubject();
-        $viewConfig = $subject->getViewConfigVerify();
-
-        $results = [ ];
-//        $old_data = $model->split( $form->getValidationGroup() );
-
-        //Это жесть конечно и забавно, но на время сойдет :)
-        $model_bind = $model->toArray();
-        foreach ( $model_bind as $_k => $_v )
-        {
-            if ( substr( $_k, -4 ) == '_dtm' )
-            {
-                $model->$_k = str_replace( ' ', 'T', $_v );
-            }
-        }
-        //Конец жести
-
-        $request = $subject->getParams()->getController()->getRequest();
-        if ( $request->isPost() )
-        {
-            $post = $request->getPost();
-
-            $attach_to       = $model->attach_to;
-            $attach_to_names = $model->attach_to_names;
-            if ( !empty( $post[ 'attach_lead' ] ) )
-            {
-                $leads      = explode( ',', $post[ 'attach_lead' ] );
-                $leadsnames = [ ];
-                foreach ( $leads as $_lead )
-                {
-                    $tmp           = $subject->getGatewayServiceVerify()->get( 'Lead' )->findOne( [ '_id' => $_lead ] );
-                    $leadsnames[ ] = $tmp->title;
-                }
-            }
-            else
-            {
-                $leads      = $attach_to[ 'Lead' ];
-                $leadsnames = $attach_to_names[ 'Lead' ];
-            }
-            if ( !empty( $post[ 'attach_patient' ] ) )
-            {
-                $patients      = explode( ',', $post[ 'attach_patient' ] );
-                $patientsnames = [ ];
-                foreach ( $patients as $_patient )
-                {
-                    $tmp              =
-                        $subject->getGatewayServiceVerify()->get( 'Patient' )->findOne( [ '_id' => $_patient ] );
-                    $patientsnames[ ] = $tmp->title;
-                }
-            }
-            else
-            {
-                $patients      = $attach_to[ 'Patient' ];
-                $patientsnames = $attach_to_names[ 'Patient' ];
-            }
-            $attach_to[ 'Lead' ]          = $leads;
-            $attach_to[ 'Patient' ]       = $patients;
-            $attach_to_names[ 'Lead' ]    = $leadsnames;
-            $attach_to_names[ 'Patient' ] = $patientsnames;
-
-            $exchange = $model->getDataModel();
-            $exchange->attach_to          = $attach_to;
-            $exchange->attach_to_names    = $attach_to_names;
-            $model->setDataModel($exchange);
-        }
-
-        $data[ 'model' ] = $model;
-        $subject->setData( $data );
     }
 
 }
