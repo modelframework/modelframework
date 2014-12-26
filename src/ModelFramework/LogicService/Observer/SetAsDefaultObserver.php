591696ea754480d6dba79fd1a17f4a7df6b379b7
s.kozar <svyatoslav.kozar@gmail.com>
2014-12-26 13:32:28 +0200

    created observer to set settings as default among others

---
+++ src/ModelFramework/LogicService/Observer/SetAsDefaultObserver.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Class ConcatenationObserver
+ * @package ModelFramework\ModelViewService
+ * @author  Vladimir Pasechnik vladimir.pasechnik@gmail.com
+ * @author  Stanislav Burikhin stanislav.burikhin@gmail.com
+ */
+
+namespace ModelFramework\LogicService\Observer;
+
+class ConcatenationObserver extends AbstractObserver
+{
+
+    public function process( $model, $key, $value )
+    {
+        $concatValue = '';
+        foreach ( $value as $value_key )
+        {
+            if ( !isset( $model->$value_key ) )
+            {
+                throw new \Exception( 'Field ' . $value_key . ' does not exist in model ' .
+                                      $model->getModelName() );
+            }
+            if ( strlen( $concatValue ) )
+            {
+                $concatValue .= ' ';
+
+            }
+            $concatValue .= $model->$value_key;
+        }
+        $model->$key = $concatValue;
+    }
+
+}
