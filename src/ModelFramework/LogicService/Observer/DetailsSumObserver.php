338b96fbea1f95eb3db0b77c426472cbbf18d767
Vladimir Pasechnik <v.pasechnik@cronagency.com>
2015-01-08 21:46:49 +0200

    ds

--- src/ModelFramework/LogicService/Observer/DetailsSumObserver.php
+++ src/ModelFramework/LogicService/Observer/DetailsSumObserver.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Class DateObserver
+ *
  * @package ModelFramework\ModelViewService
  * @author  Vladimir Pasechnik vladimir.pasechnik@gmail.com
  * @author  Stanislav Burikhin stanislav.burikhin@gmail.com
@@ -8,24 +9,25 @@
 
 namespace ModelFramework\LogicService\Observer;
 
-
 class DetailsSumObserver extends AbstractConfigObserver
 {
+
     public function process($model, $key, $value)
     {
         $subject = $this->getSubject();
 
-        $query =
-            $subject->getQueryServiceVerify()
-                    ->get($value[ 'query' ])
-                    ->setParamSource($model)
-                    ->process();
+        $query
+            = $subject->getQueryServiceVerify()
+            ->get($value['query'])
+            ->setParamSource($model)
+            ->process();
 
         $model->$key = 0;
-        $details     = $this->getSubject()->getGatewayService()->get($query->getModelName())
-                            ->find($query->getWhere(), $query->getOrder());
+        $details     = $this->getSubject()->getGatewayService()
+            ->get($query->getModelName())
+            ->find($query->getWhere(), $query->getOrder());
         foreach ($details as $detail) {
-            $model->$key += $detail->$value[ 'field' ];
+            $model->$key += $detail->$value['field'];
         }
     }
 }
