7d4bab9bb583c76d802e037c82786e3d6535f7ac
s.kozar <svyatoslav.kozar@gmail.com>
2015-01-13 20:15:20 +0200

    link observer implementation started

--- src/ModelFramework/LogicService/Observer/LinkObserver.php
+++ src/ModelFramework/LogicService/Observer/LinkObserver.php
@@ -10,14 +10,15 @@
 namespace ModelFramework\LogicService\Observer;
 
 use ModelFramework\AclService\AclDataModel;
+use ModelFramework\ConfigService\ConfigAwareInterface;
 use ModelFramework\ConfigService\ConfigAwareTrait;
 use ModelFramework\LogicService\Logic;
 use ModelFramework\Utility\SplSubject\SubjectAwareInterface;
 use ModelFramework\Utility\SplSubject\SubjectAwareTrait;
 use Zend\Db\ResultSet\ResultSetInterface;
 
-abstract class LinkObserver
-    implements \SplObserver, SubjectAwareInterface
+class LinkObserver
+    implements \SplObserver, SubjectAwareInterface, ConfigAwareInterface
 {
 
     use SubjectAwareTrait;
@@ -25,7 +26,7 @@ abstract class LinkObserver
 
     protected $linkSettings = [
         [
-            'to'   => [
+            'to'     => [
                 'model'       => 'Mail',
                 'search'      => [
                     'to',
@@ -34,15 +35,37 @@ abstract class LinkObserver
                 'storage'     => 'link_storage',
                 'title_field' => 'link_view'
             ],
-            'from' => [
-                'models'  => 'Lead',
-                'search'  => 'email',
-                'storage' => 'email_id',
-                'title'   => [
-                    'title',
-                    'email'
+            'from'   => [
+                'Lead'    => [
+                    'search'  => 'email',
+                    'storage' => 'email_id',
+                    'title'   => [
+                        'title',
+                        'email'
+                    ]
+                ],
+                'Patient' => [
+                    'search'  => 'email',
+                    'storage' => 'email_id',
+                    'title'   => [
+                        'title',
+                        'email'
+                    ]
+                ],
+                'Account' => [
+                    'search'  => 'email',
+                    'storage' => 'email_id',
+                    'title'   => [
+                        'title',
+                        'email'
+                    ]
                 ]
-            ]
+            ],
+            'models' => [
+                'Lead',
+                'Patient',
+                'Account'
+            ],
         ]
     ];
 
@@ -54,7 +77,71 @@ abstract class LinkObserver
     public function update( \SplSubject $subject )
     {
         $this->setSubject( $subject );
+        $models = $subject->getEventObject();
+        if (!is_array( $models )) {
+            $models = [ $models ];
+        }
+        foreach ($models as $model) {
+            $configs = [ ];
+            array_walk( $this->$linkSettings, function ( &$config, $key ) use ( &$configs, $model ) {
+                if (in_array( $model->getModelName(), $config[ 'models' ] )) {
+                    $configs[ ] = $config;
+                }
+            } );
+            //update as link model
+            switch ($this->getRootConfig()[ 'action' ]) {
+                case 'update':
+                    foreach ($configs as $config) {
+                        $this->updateLinkAction( $model, $config );
+                    }
+                    break;
+                case 'delete':
+                    foreach ($configs as $config) {
+                        $this->deleteLinkAction( $model, $config );
+                    }
+                    break;
+            }
+            //update as collector model
+            switch ($this->getRootConfig()[ 'action' ]) {
+                case 'update':
+                    foreach ($configs as $config) {
+                        $this->updateCollectorAction( $model, $config );
+                    }
+                    break;
+                case 'delete':
+                    foreach ($configs as $config) {
+                        $this->deleteCollectorAction( $model, $config );
+                    }
+                    break;
+            }
+            exit;
+        }
+    }
+
+    public function updateLinkAction( $model, $config )
+    {
+
         prn( $this->linkSettings );
         exit;
     }
+
+    public function deleteLinkAction( $model, $config )
+    {
+        prn( $this->linkSettings );
+        exit;
+    }
+
+    public function updateCollectorAction( $model, $config )
+    {
+
+        prn( $this->linkSettings );
+        exit;
+    }
+
+    public function deleteCollectorAction( $model, $config )
+    {
+        prn( $this->linkSettings );
+        exit;
+    }
+
 }
