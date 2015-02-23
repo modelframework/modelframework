e10c35cfbd3a5e6b8c727fb360233a4e625fac1c
Artem Bondarenko <a.bondarenko@cronagency.com>
2015-02-23 18:40:25 +0200

    multimodel pattern

--- src/ModelFramework/ViewService/Observer/ConstructPatternMenuObserver.php
+++ src/ModelFramework/ViewService/Observer/ConstructPatternMenuObserver.php
@@ -16,21 +16,32 @@ namespace ModelFramework\ViewService\Observer;
  */
 class ConstructPatternMenuObserver implements \SplObserver
 {
+    private $allow_models;
+
     public function update(\SplSubject $subject)
     {
+        $this->allow_models[] = 'Account';
 
-        $Recipient_id = $subject->getParam('recipient',0 );
-        if (!$Recipient_id){
+
+        $Recipient_id = $subject->getParam('recipient', 0);
+        if ($Recipient_id) {
+            $dataModel = ucfirst($subject
+                ->getGatewayServiceVerify()
+                ->get('Email')
+                ->findOne(['model_id' => $Recipient_id])->data);
+            $this->allow_models[] = $dataModel;
+        }
+
+        if (!is_array($this->allow_models)) {
             return;
         }
-        $dataModel = ucfirst($subject
-            ->getGatewayServiceVerify()
-            ->get('Email')
-            ->findOne(['model_id'=>$Recipient_id])->data);
-        $visibleFields['pattern_items'][$dataModel] = $subject
-             ->getAclServiceVerify()
-             ->getVisibleFields($dataModel);
 
+        foreach ($this->allow_models as $model) {
+
+            $visibleFields['pattern_items'][$model] = $subject
+                ->getAclServiceVerify()
+                ->getVisibleFields($model);
+        }
         $subject->setData($visibleFields);
     }
 }
