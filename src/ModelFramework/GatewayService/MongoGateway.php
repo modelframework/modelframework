f815d9e19c1e2bfc6cc64998a44b569fc5b9d33b
Stanislav Burikhin <s.burikhin@cronagency.com>
2015-03-10 14:23:42 +0200

    MongoGateway upd

--- src/ModelFramework/GatewayService/MongoGateway.php
+++ src/ModelFramework/GatewayService/MongoGateway.php
@@ -19,18 +19,36 @@ class MongoGateway extends MongoZendGateway
      * @return array|bool|int|mixed
      * @throws \Exception
      */
-    public function save($model)
+    public function save( $model )
     {
         if ($model instanceof DataModelInterface) {
-            if ( !$this->isUnique($model)) {
-                throw new \Exception('Data is not unique');
+            if (!$this->isUnique( $model )) {
+                throw new \Exception( 'Data is not unique' );
             }
             $data = $model->getArrayCopy();
-            $result = parent::save($data);
-            $model->_id = $data['_id'];
+            $data = $this->_convertids( $data );
+            $_id  = $model->_id;
+            if (empty( $_id )) {
+                unset( $data[ '_id' ] );
+                $insertResult = $this->insert( $data );
+                $result       = empty( $insertResult[ 'ok' ] ) ? 0
+                    : $insertResult[ 'ok' ];
+                if ($result) {
+                    $model->_id = '' . $data[ '_id' ];
+                }
+            } else {
+                if ($this->get( $_id )) {
+                    $result
+                        = $this->update( $data,
+                        $this->_where( [ '_id' => $_id ] ) );
+                } else {
+                    throw new \Exception( 'Model id does not exist' );
+                }
+            }
+
             return $result;
-        } elseif (is_array($model)) {
-            return parent::save($model);
+        } elseif (is_array( $model )) {
+            return parent::save( $model );
         }
 
         return 0;
@@ -41,16 +59,16 @@ class MongoGateway extends MongoZendGateway
      *
      * @return bool|mixed
      */
-    public function isUnique(DataModelInterface $model)
+    public function isUnique( DataModelInterface $model )
     {
         $modelConfig = $this->getParsedModelConfig();
         if ($modelConfig !== null) {
             foreach ($modelConfig->unique as $_unique) {
-                $_data = [];
-                foreach ((array)$_unique as $_key) {
-                    $_data[$_key] = $model->$_key;
+                $_data = [ ];
+                foreach ((array) $_unique as $_key) {
+                    $_data[ $_key ] = $model->$_key;
                 }
-                $check = $this->find($_data);
+                $check = $this->find( $_data );
                 if ($check->count() > 0
                     && $check->current()->id() != $model->id()
                 ) {
